969c9a69516fbd1996e3c75f14e3343d31378dbc
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-14 00:08:02 +0200

    tentando exercicio 8 com ajuda

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -14,7 +15,30 @@ def search_by_title(title) -> 'list[dict]':
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    empty_list = []
+
+    try:
+        iso_date = '%Y-%m-%d'
+        db_date = '%d/%m/%Y'
+
+        correct_date = datetime.strptime(date, iso_date).strftime(db_date)
+        # data = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+        query_correct_date = {'timestamp': correct_date}
+
+        news_list = search_news(query_correct_date)
+
+        for new_date in news_list:
+            empty_list.append((new_date['title'], new_date['url']))
+        return empty_list
+
+    except ValueError:
+        raise ValueError(
+            'Data inválida'
+        )
+
+    finally:
+        'OK'
 
 
 # Requisito 9
