b019b79d6c08e8729fc3a78fc067734026c3dde6
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-13 23:53:44 +0200

    tentando exercicio 7 com ajuda

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title) -> 'list[dict]':
+    search_title = {'title': {"$regex": title, '$options': 'i'}}
+    new_title_list = search_news(search_title)
+    empty_list = []
+
+    for new_title in new_title_list:
+        empty_list.append((new_title['title'], new_title['url']))
+    return empty_list
 
 
 # Requisito 8
