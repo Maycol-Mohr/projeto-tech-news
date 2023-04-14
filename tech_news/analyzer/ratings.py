17ac8dfa0de4ad9b07a5b8398ac965f47c96b9df
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-14 21:57:51 +0200

    tentando 10 - 11 - 12 com ajuda

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,19 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = []
+
+    for category in find_news():
+        categories.append(category['category'])
+
+    counter = Counter(sorted(categories))
+    sorted_monst_commom = counter.most_common(5)
+
+    categories_sorted = []
+
+    for category in sorted_monst_commom:
+        categories_sorted.append(category[0])
+    return categories_sorted
--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,39 @@
-# Requisitos 11 e 12
+import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import search_by_title
+from tech_news.analyzer.search_engine import search_by_date
+from tech_news.analyzer.search_engine import search_by_category
+from tech_news.analyzer.ratings import top_5_categories
+
+
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    opcao = input(
+        'Selecione uma das opções a seguir:\n'
+        ' 0 - Popular o banco com notícias;\n'
+        ' 1 - Buscar notícias por título;\n'
+        ' 2 - Buscar notícias por data;\n'
+        ' 3 - Buscar notícias por categoria;\n'
+        ' 4 - Listar top 5 categorias;\n'
+        ' 5 - Sair.'
+    )
+
+    match opcao:
+        case '0':
+            amount = input('Digite quantas notícias serão buscadas:')
+            get_tech_news(int(amount))
+        case '1':
+            title = input('Digite o título:')
+            search_by_title(title)
+        case '2':
+            date = input('Digite a data no formato aaaa-mm-dd:')
+            search_by_date(date)
+        case '3':
+            category = input('Digite a categoria:')
+            search_by_category(category)
+        case '4':
+            top_5_categories()
+        case '5':
+            print('Encerrando script')
+        case _:
+            sys.stderr.write('Opção inválida\n')
+        # sys.stderr.write("Mensagem de erro aqui\n")
